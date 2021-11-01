8cfbc5db1f3ed4ee020caed131a3aaac3d6d4d4d
sisifodev <manuelperdomo1710@icloud.com>
2021-11-01 16:07:06 -0500

    Close icon

--- src/App.js
+++ src/App.js
@@ -14,18 +14,18 @@ function App() {
   return (
     <section className="modal container">
       <button onClick={showModal} className="modal__button" id="open-modal">
-        Open Modal
+        Show Modal
       </button>
 
       <div className="modal__container" id="modal-container">
         <div className="modal__content">
           <div className="modal__close close-modal" title="Close">
-            <i className="bx bx-x"></i>
+            <img src="img/x.svg" alt="" clasName="close-icon" />
           </div>
 
-          <img src="img/star-trophy.png" alt="" class="modal__img" />
-
           <h1 className="modal__title">Good Job!</h1>
+          <img src="img/trophy.png" alt="" class="modal__img" />
+
           <p className="modal__description">Click the button to close</p>
 
           <button className="modal__button modal__button-width">
