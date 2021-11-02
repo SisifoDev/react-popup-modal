438c4305cf4d76d562f2db7787780a52500ff9ef
sisifodev <manuelperdomo1710@icloud.com>
2021-11-02 12:26:25 -0500

    Added Title

--- src/App.js
+++ src/App.js
@@ -14,39 +14,45 @@ function App() {
   };
 
   return (
-    <section className="modal container">
-      <button onClick={showModal} className="modal__button" id="open-modal">
-        Show Modal
-      </button>
+    <>
+      <header>
+        <h1>Responsive React Popup Modal</h1>
+      </header>
 
-      <div className="modal__container" id="modal-container">
-        <div className="modal__content">
-          <div
-            onClick={hideModal}
-            className="modal__close close-modal"
-            title="Close"
-          >
-            <MdClear />
-            {/* <img src="img/x.svg" alt="" clasName="close-icon" /> */}
+      <section className="modal container">
+        <button onClick={showModal} className="modal__button" id="open-modal">
+          Show Modal
+        </button>
+
+        <div className="modal__container" id="modal-container">
+          <div className="modal__content">
+            <div
+              onClick={hideModal}
+              className="modal__close close-modal"
+              title="Close"
+            >
+              <MdClear />
+              {/* <img src="img/x.svg" alt="" clasName="close-icon" /> */}
+            </div>
+
+            <h1 className="modal__title"> Good Job! 🎉</h1>
+            <p className="modal__description">Click the button to close</p>
+            <img src="img/trophy.png" alt="" class="modal__img" />
+
+            <button className="modal__button modal__button-width">
+              View status
+            </button>
+
+            <button
+              onClick={hideModal}
+              className="modal__button-link close-modal"
+            >
+              Close
+            </button>
           </div>
-
-          <h1 className="modal__title"> Good Job! 🎉</h1>
-          <p className="modal__description">Click the button to close</p>
-          <img src="img/trophy.png" alt="" class="modal__img" />
-
-          <button className="modal__button modal__button-width">
-            View status
-          </button>
-
-          <button
-            onClick={hideModal}
-            className="modal__button-link close-modal"
-          >
-            Close
-          </button>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
